d7406983edd3d591a92b9bc47a3c0d8a00a0c447
quartox <jessewlord@gmail.com>
2015-01-05 10:43:21 -0800

    added initialization of toy class

---
+++ toy.py
@@ -0,0 +1,8 @@
+__author__="Jesse Lord"
+__date__="January 1, 2015"
+
+class toy:
+    def __init__(self,id,time,duration):
+        self.id = id
+        self.time = time
+        self.duration = duration
